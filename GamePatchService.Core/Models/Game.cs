095ce0986751cfabfd225345e82b3ded1a719e3b
Khayre <khayreahmed@gmail.com>
2025-11-04 20:05:00 -0500

    Game model

---
+++ GamePatchService.Core/Models/Game.cs
@@ -0,0 +1,11 @@
+namespace GamePatchService.Core.Models;
+
+public class Game
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Publisher { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+
+    public List<GameVersion> Versions { get; set; } = new();
+}
