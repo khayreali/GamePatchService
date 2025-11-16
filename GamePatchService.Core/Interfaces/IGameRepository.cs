e4452c8b30413f5e11c6d36dd7578271a5d0948d
Khayre <khayreahmed@gmail.com>
2025-11-16 14:10:00 -0500

    IGameRepository

---
+++ GamePatchService.Core/Interfaces/IGameRepository.cs
@@ -0,0 +1,13 @@
+using GamePatchService.Core.Models;
+
+namespace GamePatchService.Core.Interfaces;
+
+public interface IGameRepository
+{
+    Task<Game?> GetByIdAsync(int id);
+    Task<Game?> GetByIdWithVersionsAsync(int id);
+    Task<IEnumerable<Game>> GetAllAsync();
+    Task<Game> AddAsync(Game game);
+    Task UpdateAsync(Game game);
+    Task DeleteAsync(int id);
+}
